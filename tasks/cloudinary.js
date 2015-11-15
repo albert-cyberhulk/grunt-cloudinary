99a2b76f2aa5ea926b064208c8b1e238ff51353e
Albert-Cyberhulk <albstepanyan@gmail.com>
2015-11-15 18:08:31 +0100

    Adding cloudinary as dependancy

--- tasks/cloudinary.js
+++ tasks/cloudinary.js
@@ -48,3 +48,35 @@ module.exports = function(grunt) {
   });
 
 };
+
+/*
+'use strict';
+
+var cloudinary = require('cloudinary');
+
+
+
+module.exports = function (grunt) {
+//
+    grunt.registerMultiTask('cloudinary', 'Upload images to Cloudinary', function () {
+
+        cloudinary.config(this.options());
+        var done = this.async();
+        cloudinary.api.resources(function (result) {
+            console.log(result);
+        });
+
+        var data = [];
+
+        this.filesSrc.forEach(function (imgSrc) {
+
+            cloudinary.uploader.upload(imgSrc, function (result) {
+                data[imgSrc] = result.url;
+                //console.log(result.url);
+                grunt.log.writeln('Uploading ' + result.url);
+            }, {public_id: imgSrc});
+
+        });
+    });
+};
+*/
