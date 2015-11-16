12826db10124c903315bdcbf6f92cec91571e146
Albert-Cyberhulk <albstepanyan@gmail.com>
2015-11-16 23:05:11 +0100

    Looping through files

--- tasks/cloudinary.js
+++ tasks/cloudinary.js
@@ -6,18 +6,38 @@
  * Licensed under the MIT license.
  */
 
+// Requiring path module
+var path = require('path');
+// Requiring async module
+var async = require('async');
 // Requiring cloudinary as a node module
 var cloudinary = require('cloudinary');
-
+// Requiring underscore module
+var _ = require('underscore');
 // Exporting Grunt module
 module.exports = function (grunt) {
+
+  function notifyForceContinue(msg) {
+    grunt.fail.warn(msg);
+  }
+
   // Registering module as a multitask
   grunt.registerMultiTask('cloudinary', 'Upload images to Cloudinary', function () {
-    // Configuring the cloudinary by options
-    cloudinary.config(this.options());
     var done = this.async();
+    var options = this.options({
+      remove: true, // removes files after uploading
+      replace: true, // replaces references of initial files with cloud ones
+    });
+    if (this.files.length < 1) {
+      notifyForceContinue('Destination not written because no source files were provided.');
+    }
+    if (_.isEmpty(this.options().credentials)) {
+      notifyForceContinue("Cloudinary credentials are not specified");
+    }
+    cloudinary.config(options.credentials);
+    done();
     this.filesSrc.forEach(function (imgSrc) {
-      console.log(imgSrc);
+      console.log("Image source", imgSrc);
       done();
     });
   });
