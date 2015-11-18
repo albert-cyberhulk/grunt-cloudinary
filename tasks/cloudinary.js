ac477bdce311a76273cdb3a7766d8f4079781214
Albert-Cyberhulk <albstepanyan@gmail.com>
2015-11-18 19:59:50 +0100

    Up and running

--- tasks/cloudinary.js
+++ tasks/cloudinary.js
@@ -23,10 +23,14 @@ module.exports = function (grunt) {
 
   // Registering module as a multitask
   grunt.registerMultiTask('cloudinary', 'Upload images to Cloudinary', function () {
-    var done = this.async();
+    this.fileStack = [];
+    this.finishExecution = this.async();
+    this.resourceType = {
+      "resource_type": "raw",
+    };
     var options = this.options({
       remove: true, // removes files after uploading
-      replace: true, // replaces references of initial files with cloud ones
+      replace: true // replaces references of initial files with cloud ones
     });
     if (this.files.length < 1) {
       notifyForceContinue('Destination not written because no source files were provided.');
@@ -35,10 +39,26 @@ module.exports = function (grunt) {
       notifyForceContinue("Cloudinary credentials are not specified");
     }
     cloudinary.config(options.credentials);
-    done();
-    this.filesSrc.forEach(function (imgSrc) {
-      console.log("Image source", imgSrc);
-      done();
-    });
+    this.filesSrc.forEach(function (filepath) {
+      if (!grunt.file.exists(filepath)) {
+        notifyForceContinue(filepath + " does not exist in the system");
+      }
+      if (filepath.match(/\.(jpg|jpeg|png|gif)$/)) {
+        this.resourceType = {
+          "resource_type": "image",
+        };
+      }
+      try {
+        cloudinary.uploader.upload(filepath, function(result, error) {
+          console.log("File " + filepath + " uploaded to" + result.url);
+          this.fileStack.push(filepath);
+          if (this.filesSrc.length === this.fileStack.length) {
+            this.finishExecution(true);
+          }
+        }.bind(this), this.resourceType);
+      } catch(ex) {
+        notifyForceContinue(ex);
+      }
+    }.bind(this));
   });
 };
